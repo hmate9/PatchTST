d47c7d11a9c33668ff89598da05e55c978cf161d
Mate Hegedus <matehegedus19@gmail.com>
2023-07-02 19:40:22 +0200

    btc

---
+++ PatchTST_supervised/preprocess_btc.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import ta
+
+df = pd.read_csv('../BTCUSDT_1h.csv')
+
+# Drop the first 10% of rows
+df = df.iloc[int(len(df)*0.1):].reset_index(drop=True)
+
+df['gain'] = df['close'] / df['open'] - 1
+df['ibs'] = (df['close'] - df['low']) / (df['high'] - df['low'])
+df['volume_ma_14'] = df['volume'].rolling(14).mean()
+df['volume_ratio'] = df['volume'] / df['volume_ma_14'].shift(1)
+df['size'] = (df['high'] - df['low']) / df['close']
+
+def add_features(df):
+
+  float_cols = ['open', 'high', 'low', 'close', 'volume']
+  df[float_cols] = df[float_cols].astype(float)
+
+  df['rsi'] = ta.momentum.stochrsi(df['close'], window=14)
+
+  df['adx'] = ta.trend.ADXIndicator(df['high'], df['low'], df['close'], window=14).adx()
+
+  df['tsi'] = ta.momentum.TSIIndicator(df['close']).tsi()
+
+  df['atr'] = ta.volatility.AverageTrueRange(df['high'], df['low'], df['close']).average_true_range()
+
+  df['gain'] = df['close'] / df['open'] - 1
+  df['ibs'] = (df['close'] - df['low']) / (df['high'] - df['low'])
+
+  df['close_10'] = df['close'].rolling(10).mean()
+  df['close_10_diff'] = df['close'] / df['close_10'] - 1
+
+  df['close_20'] = df['close'].rolling(20).mean()
+  df['close_20_diff'] = df['close'] / df['close_20'] - 1
+
+  df['close_50'] = df['close'].rolling(50).mean()
+  df['close_50_diff'] = df['close'] / df['close_50'] - 1
+
+  df['volume_20_diff'] = df['volume'] / df['volume'].rolling(20).mean() - 1
+
+  features = ['open', 'high', 'low', 'close', 'gain', 'ibs', 'volume', 'close_10_diff', 'close_20_diff', 'close_50_diff']
+
+  features = ['open', 'high', 'low', 'close', 'gain', 'ibs', 'close_10_diff', 'close_20_diff', 'close_50_diff', 'rsi', 'adx']
+
+  features = ['gain', 'ibs', 'close_10_diff', 'close_20_diff', 'close_50_diff', 'rsi', 'adx']
+
+
+  return features
+
+features = add_features(df)
+
+# Convert time column to datetime
+df['date'] = pd.to_datetime(df['time'], unit='ms')
+
+# Remove NA 
+df = df.dropna().reset_index(drop=True)
+
+# Select the features to use
+df = df[features + ['date']]
+
+# Save the data
+df.to_csv('../BTCUSDT_1h_features.csv', index=False)
---
+++ PatchTST_supervised/scripts/PatchTST/btc.sh
@@ -0,0 +1,48 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/btc" ]; then
+    mkdir ./logs/btc
+fi
+seq_len=240
+model_name=PatchTST
+
+root_path_name=../
+data_path_name=BTCUSDT_1h_features.csv
+model_id_name=btc
+data_name=custom
+
+random_seed=2021
+for pred_len in 3
+do
+    python3 -u run_longExp.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features MS \
+      --target 'gain' \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 862 \
+      --freq 'h' \
+      --e_layers 3 \
+      --n_heads 16 \
+      --d_model 128 \
+      --d_ff 256 \
+      --dropout 0.2\
+      --fc_dropout 0.2\
+      --head_dropout 0\
+      --patch_len 16\
+      --stride 8\
+      --des 'Exp' \
+      --train_epochs 100\
+      --patience 10\
+      --lradj 'TST'\
+      --pct_start 0.2\
+      --itr 1 --batch_size 24 --learning_rate 0.001 #>logs/btc/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+done
