d721f5f4691532a7c94eff1cf6cb21de43827e7f
Mate Hegedus <matehegedus19@gmail.com>
2023-07-06 10:10:45 +0200

    new data

--- PatchTST_supervised/preprocess_btc.py
+++ PatchTST_supervised/preprocess_btc.py
@@ -47,6 +47,8 @@ def add_features(df):
 
   features = ['gain', 'ibs', 'close_10_diff', 'close_20_diff', 'close_50_diff', 'rsi', 'adx', 'bar_size']
 
+  features = ['open', 'high', 'low', 'close', 'gain', 'ibs', 'close_10_diff', 'close_20_diff', 'close_50_diff', 'rsi', 'adx', 'bar_size']
+
 
   return features
 
